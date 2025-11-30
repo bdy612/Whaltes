eaedba4476655f1097840b2f8ad9923121b480ee
bdy612 <129867742+bdy612@users.noreply.github.com>
2025-11-30 15:56:44 +0300

    Update runner.py

--- runner.py
+++ runner.py
@@ -1,9 +1,10 @@
 import tkinter as tk
-from Files.index import EncryptionApp
+from index import EncryptionApp
 def activate():
     root = tk.Tk()
     app = EncryptionApp(root)
     root.mainloop()
 
 if __name__ == "__main__":
+
     activate()
